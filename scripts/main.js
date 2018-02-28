ab98fbf5e286e5defc5e2391379b540114f7cf1b
LEAFERx <leaferx@outlook.com>
2018-02-28 20:19:57 +0800

    update!

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,20 @@ module.exports = (robot) => {
         res.send("Nope meow~");
         setTimeout(() => { res.send("Scare you!!Meow!!"); }, 5000);
     });
+
+    robot.reply(/Meow say (.*)/i, (res) => {
+        res.send(res.match[1] + "!");
+    });
+
+    robot.hear(/w(!|！)/i, (res) => {
+        res.send("w!");
+    });
+
+    robot.hear(/233+/, (res) => {
+        res.send("哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈！");
+    });
+
+    robot.hear(/ojbk/i, (res) => {
+        res.send("O什么K？");
+    });
 }
