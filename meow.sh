1cb1df4e4b538ceacb01be90afe2f638e92c4d3e
LEAFERx <leaferx@outlook.com>
2018-03-03 09:50:34 +0800

    update meow.sh

--- meow.sh
+++ meow.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "asd"
-pkill -9 node
+set -e
+
 git pull
-npm install
-HUBOT_BEARYCHAT_TOKENS=3a7fb71fce149f73d2606e6981a2005e PORT=80 setsid bin/hubot -a bearychat
+
+export HUBOT_BEARYCHAT_TOKENS=3a7fb71fce149f73d2606e6981a2005e 
+export PORT=80 
+
+setsid bin/hubot -a bearychat
