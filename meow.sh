cee08a2ec47e9d94d1f5fa671aabb64f19200309
LEAFERx <leaferx@outlook.com>
2018-03-03 09:55:19 +0800

    put sh to bg

--- meow.sh
+++ meow.sh
@@ -3,7 +3,7 @@ set -e
 
 git pull
 
-export HUBOT_BEARYCHAT_TOKENS=3a7fb71fce149f73d2606e6981a2005e 
-export PORT=80 
+export HUBOT_BEARYCHAT_TOKENS=3a7fb71fce149f73d2606e6981a2005e
+export PORT=80
 
-setsid bin/hubot -a bearychat
+setsid bin/hubot -a bearychat &
